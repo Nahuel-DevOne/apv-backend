57a4959a60e51f1ec22177479b61f6699c70b094
Nahuel <nahuel.profesor@gmail.com>
2023-01-16 00:51:00 -0300

    Avanzando con la conexión de la base de datos

--- config/db.js
+++ config/db.js
@@ -1,10 +1,24 @@
 import mongoose from 'mongoose';
 
+// mongoose.set("strictQuery", false);
+// mongoose.connect(process.env.MONGO_URL, () => {
+//   console.log("Connected to MongoDB");
+// });
+
 const conectarDB = async () => {
     try {
-        
+        mongoose.set("strictQuery", false);
+        const db = await mongoose.connect(process.env.MONGO_URI, 
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        const url = `${db.connection.host}: ${db.connection.port}`
+        console.log(`MongoDB conectado en: ${url}`);
     } catch (error) {
-        
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
     }
 }
 
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from "express";
+import dotenv from 'dotenv';
 import conectarDB from "./config/db.js";
 
 const app = express();
+dotenv.config();
 
 conectarDB();
+console.log(process.env.MONGO_URI);
 
 app.use('/', (req, res) => {
     res.send('Hola mundo soy Goku');
 });
 
-app.listen(4000, () => {
-    console.log('Servidor corriendo en el puerto 4000');
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
