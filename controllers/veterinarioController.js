57f6906570d29193d7bdfa5dc840e185efebcef0
Nahuel <nahuel.profesor@gmail.com>
2023-01-18 19:35:14 -0300

    Confirmando la cuenta via token terminado

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -10,7 +10,7 @@ const registrar = async (req, res) => {
         const error = new Error('Usuario ya registrado');
         return res.status(400).json({ msg: error.message })
     }
-    
+
     try {
         // Guardar un nuevo veterinario
         const veterinario = new Veterinario(req.body);
@@ -27,4 +27,24 @@ const perfil = (req, res) => {
     res.json( {msg: "Mostrando perfil" })
 };
 
-export { registrar, perfil }
+const confirmar = async (req, res) => {
+    const { token } = req.params
+    const usuarioConfirmar = await Veterinario.findOne({token})
+     
+    if(!usuarioConfirmar){
+        const error = new Error('Token no válido')
+        return res.status(404).json({ msg: error.message });
+    }
+
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+        res.json({msg: 'Usuario confirmado correctamente'})
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+export { registrar, perfil, confirmar }
